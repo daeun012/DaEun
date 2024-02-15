2c0889f20771e25c7f48f3b70279cb5b46e8f4bf
Daeun Kim <daeun@si-analytics.ai>
2024-02-15 22:26:57 +0900

    feat: add skill image and set skil

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,15 +3,12 @@ import PageTitle from "../components/PageTitle/PageTitle";
 import "./About.scss";
 
 const About = () => (
-  <div className="container-about">
+  <div className="about">
     <PageTitle title="About" />
     <div className="body">
       <h2 className="name">DaEun Kim • 김다은</h2>
-
-      <h3>왜?로 시작하는 개발자</h3>
       <p>
-        <b>항상 왜?라는 의문을 품으며 학습하고 습득한 내용들을 기록합니다.</b> 아는 것 만으로는 충분하지 않다고 생각합니다. 토이 프로젝트를 통해 적용해 나가며
-        성장하고 있습니다.
+        <b>항상 왜?라는 의문을 품으며 학습하고 습득한 내용들을 기록합니다.</b> 아는 것 만으로는 충분하지 않다고 생각합니다.
       </p>
       <p>
         <b>집요합니다.</b> 개발을 하다 보면 쉽게 풀리지 않는 문제에 반드시 마주하게 됩니다. 문제를 해결하는 것에 그치지 않고, 문제가 왜 발생했고 왜 해결되었는지
@@ -23,7 +20,7 @@ const About = () => (
       <p>
         <b>한순간의 열정이 아닌 꾸준함으로 승부하겠습니다. </b>
       </p>
-      <div className="links">
+      <div className="detail-info">
         <p>이메일 : rla971203@gmail.com</p>
         <p>연락처 : 010-4116-5131</p>
         <p>
@@ -33,25 +30,21 @@ const About = () => (
           </a>
         </p>
       </div>
-      <div className="abilities">
+      <div className="skill-group">
         <div className="skill">
-          <h3>Front-end</h3>
+          <h3>Frontend</h3>
           <div className="img">
             <img src="https://daeun012.github.io/DaEun/images/web.png" alt="html" height="100" width="250" />
             <img src="https://daeun012.github.io/DaEun/images/react.png" alt="react" width="90" />
             <img src="https://daeun012.github.io/DaEun/images/redux.png" alt="redux" width="100" />
+            <img src="https://daeun012.github.io/DaEun/images/typescript.png" alt="typescript" width="100" />
           </div>
         </div>
         <div className="skill">
-          <h3>Back-end</h3>
+          <h3>Backend</h3>
           <div className="img">
-            <img src="https://daeun012.github.io/DaEun/images/nodejs.png" alt="node.js" height="100" width="100" />{" "}
+            <img src="https://daeun012.github.io/DaEun/images/nodejs.png" alt="node.js" height="100" width="100" />
             <img src="https://daeun012.github.io/DaEun/images/express.png" height="100" alt="express.js" width="150" />
-          </div>
-        </div>
-        <div className="skill">
-          <h3>Database</h3>
-          <div className="img">
             <img src="https://daeun012.github.io/DaEun/images/mysql.png" alt="mysql" height="100" width="100" style={{ marginRight: 25 }} />
             <img src="https://daeun012.github.io/DaEun/images/mongodb.png" alt="mongoDB" height="100" width="100" />
           </div>
@@ -61,12 +54,13 @@ const About = () => (
           <div className="img">
             <img src="https://daeun012.github.io/DaEun/images/git.png" alt="git" height="100" width="100" />
             <img src="https://daeun012.github.io/DaEun/images/github.png" alt="github" height="100" width="100" />
+            <img src="https://daeun012.github.io/DaEun/images/gitkraken.png" alt="gitkraken" height="100" width="100" />
           </div>
         </div>
         <div className="skill">
           <h3>Certificate</h3>
           <div className="img">
-            <img src="https://daeun012.github.io/DaEun/images/certificate.jpg" alt="certificate" height="120" width="180" />
+            <img src="https://daeun012.github.io/DaEun/images/certificate.jpg" alt="certificate" height="100" width="130" />
           </div>
         </div>
       </div>
