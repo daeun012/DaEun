5b4a3678b57fc5549338ab6a7520bcfd8e4f81a0
Daeun Kim <daeun@si-analytics.ai>
2025-07-07 20:31:58 +0900

    폰트 추가

---
+++ src/components/ButtonNavLink/ButtonNavLink.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ButtonNavLink.scss";
+
+export default function ButtonNavLink({ name }) {
+	return (
+		<div className="buttonNavLink">
+			{name === "Blog" ? (
+				<a className="link-wrapper" target="_blank" href="https://daeun012.github.io/" rel="noreferrer">
+					<div className="title">
+						{name} <div className="bar" />
+					</div>
+					<div className="arrow-wrapper">
+						<img src="https://daeun012.github.io/DaEun/images/link_arrow.svg" alt={name} />
+					</div>
+				</a>
+			) : (
+				<Link className="link-wrapper" activeclassname="selected" to={`/${name.toLowerCase()}`}>
+					<div className="title">
+						{name} <div className="bar" />
+					</div>
+					<div className="arrow-wrapper">
+						<img src="https://daeun012.github.io/DaEun/images/link_arrow.svg" alt={name} />
+					</div>
+				</Link>
+			)}
+		</div>
+	);
+}
