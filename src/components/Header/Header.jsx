89c81082ca131efcf0049528438399ccf3b5606c
Daeun Kim <daeun@si-analytics.ai>
2025-07-15 09:30:16 +0900

    design: header 디자인 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,85 @@
 import cn from "classnames";
-import { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
-import ButtonNavLink from "../navLinkButton/NavLinkButton";
+import { useEffect, useRef, useState } from "react";
 import "./Header.scss";
 import MenuButton from "./menuButton/MenuButton";
 
-export default function Header() {
+const NAV_MENU = [
+	{ idx: 0, name: "About" },
+	{ idx: 1, name: "Project" },
+	{ idx: 2, name: "Career" },
+];
+
+export default function Header({ scrollRef }) {
 	const [open, setOpen] = useState(false);
+	const [navIndex, setNavIndex] = useState(null);
+	const navRef = useRef([]); // 이동할 각각의 컴포넌트에 대응하는 목차 버튼을 저장할 ref 배열
+
+	useEffect(() => {
+		// { behavior: 'smooth' } 속성을 주면 스크롤이 스르륵~ 올라가거나 내려가면서 이동하고, 없으면 아무 애니메이션 없이 바로 목적지를 보여준다.
+		scrollRef.current[navIndex]?.scrollIntoView({ behavior: "smooth" });
+		setNavIndex(null);
+	}, [scrollRef, navIndex]);
+
+	// 현재 스크롤 위치에 따라 NavBar 버튼 스타일이 바뀌도록 클래스명을 지정한다.
+	useEffect(() => {
+		const changeNavBtnStyle = () => {
+			scrollRef.current.forEach((ref, idx) => {
+				if (ref.offsetTop - 180 < window.scrollY) {
+					navRef.current.forEach((ref) => {
+						ref.className = ref.className.replace(" active", "");
+					});
+
+					navRef.current[idx].className += " active";
+				}
+			});
+		};
+
+		window.addEventListener("scroll", changeNavBtnStyle);
+
+		return () => {
+			window.removeEventListener("scroll", changeNavBtnStyle);
+		};
+	}, [scrollRef]);
 
 	return (
-		<div className="header-container">
-			<div className="header-wrapper">
+		<header className="header">
+			<div className="container">
 				<div className="items left-side">
-					<Link className="logo-wrapper" to="/">
-						DAEUN
-					</Link>
+					<button className="logo">DaEun's Portfolio</button>
 				</div>
 				<div className="items right-side">
+					<nav className="menu">
+						{NAV_MENU.map(({ idx, name }) => (
+							<button
+								key={idx}
+								className={"menu-item"}
+								ref={(ref) => (navRef.current[idx] = ref)}
+								onClick={() => {
+									setNavIndex(idx);
+								}}
+							>
+								{name}
+							</button>
+						))}
+					</nav>
 					<MenuButton open={open} toggleMenu={() => setOpen((prev) => !prev)} />
-					<div className="menu">
-						<NavLink className={({ isActive }) => cn("menu-item", isActive && "selected")} to="/about" end>
-							About _
-						</NavLink>
-						<NavLink className={({ isActive }) => cn("menu-item", isActive && "selected")} to="/career" end>
-							Career _
-						</NavLink>
-						<NavLink className={({ isActive }) => cn("menu-item", isActive && "selected")} to="/project" end>
-							Project _
-						</NavLink>
-						<a className="menu-item" target="_blank" href="https://daeun012.github.io/" rel="noreferrer">
-							Blog _
-						</a>
-					</div>
-
-					<CSSTransition classNames="mobile-menu" in={open} timeout={500} unmountOnExit>
-						<div className="mobile-menu-wrapper" onClick={() => setOpen(false)}>
-							<div className="menu-buttons-group">
-								<ButtonNavLink name="About" />
-								<ButtonNavLink name="Career" />
-								<ButtonNavLink name="Project" />
-								<ButtonNavLink name="Blog" />
-							</div>
-						</div>
-					</CSSTransition>
 				</div>
 			</div>
-		</div>
+
+			<div className={cn("mobile-menu-wrapper", { isOpen: open })} onClick={() => setOpen((prev) => !prev)}>
+				{NAV_MENU.map(({ idx, name }) => (
+					<button
+						key={idx}
+						className={"mobile-menu-item"}
+						ref={(ref) => (navRef.current[idx] = ref)}
+						onClick={() => {
+							setNavIndex(idx);
+						}}
+					>
+						{name}
+					</button>
+				))}
+			</div>
+		</header>
 	);
 }
--- src/components/Header/MenuButton/MenuButton.jsx
+++ src/components/Header/MenuButton/MenuButton.jsx
@@ -1,16 +1,15 @@
 import classNames from "classnames";
-import React from "react";
 import "./MenuButton.scss";
 
 export default function MenuButton({ open, toggleMenu }) {
 	return (
-		<div className={classNames("menuIcon-container", { active: open })} onClick={toggleMenu}>
+		<button className={classNames("menuIcon-container", { active: open })} onClick={toggleMenu}>
 			<div className="menuIcon-wrapper">
 				<div className="bar top" />
 				<div className="bar mid first" />
 				<div className="bar mid second" />
 				<div className="bar bottom" />
 			</div>
-		</div>
+		</button>
 	);
 }
