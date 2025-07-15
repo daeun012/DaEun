37d760505d81019ffd1c5500c9a313115fbb49f0
Daeun Kim <daeun@si-analytics.ai>
2025-07-16 00:20:14 +0900

    fix: class명 변경

--- src/components/sectionTitle/SectionTitle.jsx
+++ src/components/sectionTitle/SectionTitle.jsx
@@ -1,5 +1,5 @@
 import "./SectionTitle.scss";
 
 export default function SectionTitle({ title }) {
-	return <div className="page-title">{title}</div>;
+	return <div className="section-title">{title}</div>;
 }
